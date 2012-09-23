a34a2846dedc56aa0b03e598eafc0633d6f49ead
Roland Yonaba <roland.yonaba@gmail.com>
2012-09-23 16:21:49 +0000

    loader.externalFont renamed to loader.extFont loader.streamingAudio renamed to loader.Audio.Stream loader.staticAudio renamed to loader.Audio.static Bugfixes

--- Loader/loader.lua
+++ Loader/loader.lua
@@ -24,7 +24,7 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 assert(love, 'Love 2D framework is required')
 
 -- loader version
-local _VERSION = "0.1"
+local _VERSION = "0.2"
 
 -- Internalization
 local foreach = table.foreach
@@ -122,16 +122,9 @@ loader.getBaseImageDir = function() return DefaultBaseImagePath end
 loader.getBaseAudioDir = function() return DefaultBaseAudioPath end
 loader.getBaseFontSize = function() return DefaultBaseFontSize end
 
-
-
-
-
-
-
-
 function loader.init()  
   -- Access to Love's default font (Vera.ttf)
-  if love._.modules.graphics then
+  if love._modules.graphics then
     loader.Font = {}
     loader.Font = setmetatable({}, {__index = function(self,k)
       assert(type(k) == 'number' and floor(k) == k, ('Wrong argument type. Number expected, got %s'):format(type(k)))
@@ -144,24 +137,23 @@ function loader.init()
     end})
   
     -- Custom *.ttf font loading 
-    loader.externalFont = {}
+    loader.extFont = {}
     foreach(love.filesystem.enumerate(DefaultBaseExternalFontPath), function(_,font)
     local f = font:match('(.+)%.ttf$')
       if f then
-        loader.externalFont[f] = setmetatable({__fontFile = font},baseExtFontMetatable)
+        loader.extFont[f] = setmetatable({__fontFile = font},baseExtFontMetatable)
       end    
     end)
   end
-  
   -- Image loading
-  if love._.modules.graphics and love._modules.image then
+  if love._modules.graphics and love._modules.image then
     loader.Image = {}
     loader.Image = recurseSetImgMeta(getFolderTree(DefaultBaseImagePath))
   end
   -- Load static audio sources
   if love._modules.audio and love._modules.sound then
-    loader.staticAudio = {}
-    loader.staticAudio = setmetatable({}, {__index = function(self,k)
+    loader.Audio = {}
+    loader.Audio.Static = setmetatable({}, {__index = function(self,k)
       self[k] = love.audio.newSource(getFilePath(k,DefaultBaseAudioPath,DefaultBaseAudioFormats),'static')
       return self[k]
       end,
@@ -170,8 +162,7 @@ function loader.init()
     end})
 
     -- Load streaming audio sources
-    loader.streamingAudio = {}
-    loader.streamingAudio = setmetatable({}, {__index = function(self,k)
+    loader.Audio.Stream = setmetatable({}, {__index = function(self,k)
       self[k] = love.audio.newSource(getFilePath(k,DefaultBaseAudioPath,DefaultBaseAudioFormats),'stream')
       return self[k]
       end,
@@ -179,6 +170,8 @@ function loader.init()
         return self[k] 
     end})
   end
+  
 end
+
 
 return loader
--- version_history.md
+++ version_history.md
@@ -1,4 +1,7 @@
 #Version history#
 
+##0.2 (09/23/12)
+* Initial Release
+
 ##0.1 (09/04/12)
 * Initial Release
